b6438b1af5078876808cb04687bdc9970be16745
jeffbernath <jeffreybernath@gmail.com>
2026-08-26 03:28:18 -0600

    Update Coffee Controller and add gitignore

--- Pico_Coffee/Pico_Coffee.c
+++ Pico_Coffee/Pico_Coffee.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include "pico/bootrom.h"
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
 #include "hardware/i2c.h"
@@ -61,6 +63,39 @@ int64_t alarm_callback(alarm_id_t id, void *user_data) {
 #define UART_TX_PIN 4
 #define UART_RX_PIN 5
 
+void check_usb_commands(void)
+{
+    static char buffer[32];
+    static int index = 0;
+
+    int ch;
+
+    while ((ch = getchar_timeout_us(0)) != PICO_ERROR_TIMEOUT)
+    {
+        if (ch == '\n' || ch == '\r')
+        {
+            if (index > 0)
+            {
+                buffer[index] = '\0';
+
+                if (strcmp(buffer, "BOOTSEL") == 0)
+                {
+                    printf("Entering BOOTSEL...\n");
+                    stdio_flush();
+                    sleep_ms(100);
+
+                    reset_usb_boot(0, 0);
+                }
+
+                index = 0;
+            }
+        }
+        else if (index < sizeof(buffer) - 1)
+        {
+            buffer[index++] = (char)ch;
+        }
+    }
+}
 
 
 int main()
@@ -124,7 +159,7 @@ int main()
     printf("Loaded program at %d\n", offset);
     
     #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
+    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 1);
     #else
     blink_pin_forever(pio, 0, offset, 6, 3);
     #endif
@@ -151,10 +186,10 @@ int main()
     
     // Enable the watchdog, requiring the watchdog to be updated every 100ms or the chip will reboot
     // second arg is pause on debug which means the watchdog will pause when stepping through code
-    watchdog_enable(100, 1);
+    //watchdog_enable(100, 1);
     
     // You need to call this function at least more often than the 100ms in the enable call to prevent a reboot
-    watchdog_update();
+    //watchdog_update();
 
     printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
     printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
@@ -176,7 +211,14 @@ int main()
     // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
 
     while (true) {
+        check_usb_commands();
+
         printf("Hello, world!\n");
-        sleep_ms(1000);
+
+        for (int i = 0; i < 100; i++) {
+            check_usb_commands();
+            watchdog_update();
+            sleep_ms(10);
+        }
     }
 }
